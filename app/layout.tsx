170764e83cad90aa736ad90d4b945ef163e00b20
themagicdev3351 <themagicdev3351@gmail.com>
2024-09-02 16:17:50 +0530

    changes 02-09

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { auth } from "@/auth";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/sonner";
+import Navbar from "./(public)/_components/navbar";
+import FooterNav from "./(public)/_components/footerNav";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,8 +26,16 @@ export default async function RootLayout({
     <SessionProvider session={session}>
       <html lang="en">
         <body className={inter.className}>
+          <Navbar />
           <Toaster />
-          {children}
+          <main
+            className={cn(
+              " h-[calc(100vh-100px)] fixed top-[50px] w-full bg-gradient"
+            )}
+          >
+            {children}
+          </main>
+          <FooterNav />
         </body>
       </html>
     </SessionProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -9,31 +9,26 @@ const poppins = Poppins({
 });
 export default function Home() {
   return (
-    <main
-      className={cn(
-        "flex h-full flex-col items-center justify-center bg-gradient"
-      )}
-    >
-      <div className=" space-y-6 text-center">
-        <h1
-          className={cn(
-            " text-7xl text-sky-50 font-semibold",
-            poppins.className
-          )}
-        >
-          Auth.js
-        </h1>
-        <p className=" text-slate-400 text-lg">
-          A simple authentication service
-        </p>
-        <div>
-          <LoginButton mode="modal" asChild>
-            <Button variant="secondary" size="lg" className=" text-lg">
-              Sign in
-            </Button>
-          </LoginButton>
-        </div>
+
+    <div className=" space-y-6 text-center">
+      <h1
+        className={cn(
+          " text-7xl text-sky-50 font-semibold",
+          poppins.className
+        )}
+      >
+        Auth.js
+      </h1>
+      <p className=" text-slate-400 text-lg">
+        A simple authentication service
+      </p>
+      <div>
+        <LoginButton mode="modal" asChild>
+          <Button variant="secondary" size="lg" className=" text-lg">
+            Sign in
+          </Button>
+        </LoginButton>
       </div>
-    </main>
+    </div>
   );
 }
