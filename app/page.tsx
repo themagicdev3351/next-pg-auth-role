5254dd3ed842ef312d1fccebd3383ab767ad1293
themagicdev3351 <themagicdev3351@gmail.com>
2024-09-03 09:11:34 +0530

    slider

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,22 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 
+import DemoSlider from "./_components/DemoSlider";
+import dataSlider from "./_data/slider-data.json";
+
+
 const poppins = Poppins({
   subsets: ["latin"],
   weight: "600",
 });
+
 export default function Home() {
   return (
-
-    <div className=" space-y-6 text-center">
-      <h1
-        className={cn(
-          " text-7xl text-sky-50 font-semibold",
-          poppins.className
-        )}
-      >
-        Auth.js
-      </h1>
-      <p className=" text-slate-400 text-lg">
-        A simple authentication service
-      </p>
+    <div className={`space-y-6 text-center ${poppins.className}`}>
+      <DemoSlider data={dataSlider} />
       <div>
         <LoginButton mode="modal" asChild>
-          <Button variant="secondary" size="lg" className=" text-lg">
+          <Button variant="secondary" size="lg" className="text-lg">
             Sign in
           </Button>
         </LoginButton>
